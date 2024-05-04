82c87829c9e3419cae0db8dcb5e7fdb2170c36ff
小川哲男 <ogawatetsuo@ogawatetsuonoiMac.local>
2024-05-04 16:10:28 +0900

    second commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import axios from "axios";
 import './App.css';
+
+const onClickUsers = () => {
+  const api = "https://463mivhum3.execute-api.ap-northeast-1.amazonaws.com/default/amptest01Func01-dev";
+
+  axios
+    .get(api)
+    .then((res) => {
+      console.log(res.data);
+    })
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={onClickUsers}>users</button>
     </div>
   );
 }
