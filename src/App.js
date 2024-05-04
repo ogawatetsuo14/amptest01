9fbf576b109bc3a2d3094730b9e0e58a933772d6
小川哲男 <ogawatetsuo@ogawatetsuonoiMac.local>
2024-05-05 00:33:42 +0900

    9th commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import axios from "axios";
 import './App.css';
+import { Amplify } from '@aws-amplify';
 import { withAuthenticator, Button } from '@aws-amplify/ui-react';
 import { signOut } from 'aws-amplify/auth';
 import '@aws-amplify/ui-react/styles.css';
+
+import awsconfig from './aws-exports';
+Amplify.configure(awsconfig);
 
 const onClickUsers = () => {
   const api = "https://4dsda75wt5.execute-api.ap-northeast-1.amazonaws.com/default/amptest0127e81e2e-main";
